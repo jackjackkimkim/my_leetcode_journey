74a37011222aac64daf01cfed28b448f221cfc33
Jaeyoung  Kim <jaeyoungkim@Jaeyoungs-MacBook-Pro.local>
2022-07-02 13:44:15 +0900

    lowest

--- flood_fill.py
+++ flood_fill.py
@@ -7,33 +7,6 @@ sc1 = 1
 color1 = 2
 
 
-# class Solution:
-#     def floodFill(
-#         self, image: List[List[int]], sr: int, sc: int, color: int
-#     ) -> List[List[int]]:
-#         if image[sr][sc] == color or image == None:
-#             return image
-#         initial = image[sr][sc]
-#         self.fill(image, sr, sc, initial, color)
-#         return image
-
-#     def fill(self, image, r, c, initial, color):
-#         if (
-#             r < 0
-#             or r > len(image) - 1
-#             or c < 0
-#             or c > len(image[0]) - 1
-#             or image[r][c] != initial
-#         ):
-#             return
-
-#         image[r][c] = color
-#         self.fill(image, r + 1, c, initial, color)
-#         self.fill(image, r - 1, c, initial, color)
-#         self.fill(image, r, c + 1, initial, color)
-#         self.fill(image, r, c - 1, initial, color)
-
-
 class Solution:
     def floodFill(
         self, image: List[List[int]], sr: int, sc: int, color: int
